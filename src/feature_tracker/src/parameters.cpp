62ae662bb52f29f1af5e50ed01183d6122d15546
serkan <serkanmazlum306@gmail.com>
2024-03-21 14:36:12 +0300

    feature_tracker

---
+++ src/feature_tracker/src/parameters.cpp
@@ -0,0 +1,72 @@
+#include "parameters_.hpp"
+
+std::string IMAGE_TOPIC;
+std::string IMU_TOPIC;
+std::vector<std::string> CAM_NAMES;
+std::string FISHEYE_MASK;
+int MAX_CNT;
+int MIN_DIST;
+int WINDOW_SIZE;
+int FREQ;
+double F_THRESHOLD;
+int SHOW_TRACK;
+int STEREO_TRACK;
+int EQUALIZE;
+int ROW;
+int COL;
+int FOCAL_LENGTH;
+int FISHEYE;
+bool PUB_THIS_FRAME;
+
+int row = 680;
+int column = 480;
+double focal_length = 320.0;
+
+template <typename T>
+T readParam(rclcpp::Node &n, std::string name){
+    T ans;
+    if (n.get_parameter(name, ans)) {
+        std::cout << "Loaded " << name << ": " << ans << std::endl;
+    }
+    else {
+        std::cout << "Failed to load " << name << std::endl;
+        rclcpp::shutdown();
+    }
+    return ans;
+}
+
+void readParameters(rclcpp::Node &n){
+    std::string config_file;
+    config_file = readParam<std::string>(n, "config_file");
+    cv::FileStorage fsSettings(config_file, cv::FileStorage::READ);
+    if(!fsSettings.isOpened())
+    {
+        std::cerr << "ERROR: Wrong path to settings" << std::endl;
+    }
+    std::string VINS_FOLDER_PATH = readParam<std::string>(n, "vins_folder");
+
+    fsSettings["image_topic"] >> IMAGE_TOPIC;
+    fsSettings["imu_topic"] >> IMU_TOPIC;
+    MAX_CNT = fsSettings["max_cnt"];
+    MIN_DIST = fsSettings["min_dist"];
+    ROW = fsSettings["image_height"];
+    COL = fsSettings["image_width"];
+    FREQ = fsSettings["freq"];
+    F_THRESHOLD = fsSettings["F_threshold"];
+    SHOW_TRACK = fsSettings["show_track"];
+    EQUALIZE = fsSettings["equalize"];
+    FISHEYE = fsSettings["fisheye"];
+    if (FISHEYE == 1)
+        FISHEYE_MASK = VINS_FOLDER_PATH + "config/fisheye_mask.jpg";
+    CAM_NAMES.push_back(config_file);
+
+    WINDOW_SIZE = 20;
+    STEREO_TRACK = false;
+    FOCAL_LENGTH = 460;
+    PUB_THIS_FRAME = false;
+
+    if (FREQ == 0)
+        FREQ = 100;
+
+    fsSettings.release();
+}
