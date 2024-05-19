43ed29256bbb65d1a3e266dd3a0e3079eece81d6
ptan044 <tanpinda@gmail.com>
2024-05-19 14:16:28 +0000

    [#1] Update PoseLocalParameterization to use manifolds instead of LocalParameterization (deprecated and removed from ceres). WIP as behaviour of new Minus is unknown.

--- vins_estimator/src/factor/pose_local_parameterization.h
+++ vins_estimator/src/factor/pose_local_parameterization.h
@@ -4,10 +4,12 @@
 #include <ceres/ceres.h>
 #include "../utility/utility.h"
 
-class PoseLocalParameterization : public ceres::LocalParameterization
+class PoseManifold : public ceres::Manifold
 {
     virtual bool Plus(const double *x, const double *delta, double *x_plus_delta) const;
-    virtual bool ComputeJacobian(const double *x, double *jacobian) const;
-    virtual int GlobalSize() const { return 7; };
-    virtual int LocalSize() const { return 6; };
+    virtual bool Minus(const double *y, const double *x, double *y_minus_x) const;
+    virtual bool PlusJacobian(const double *x, double *jacobian) const;
+    virtual bool MinusJacobian(const double *x, double *jacobian) const;
+    virtual int AmbientSize() const { return 7; };
+    virtual int TangentSize() const { return 6; };
 };
