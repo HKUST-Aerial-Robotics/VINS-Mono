621ad712feeaf2b327330112de5b1ff143d13268
TanPinDa <TanPinDa@gmail.com>
2024-05-26 11:50:46 +0000

    [#5] Address PR comments. Update install deps and cmake

--- install-deps.sh
+++ install-deps.sh
@@ -2,17 +2,26 @@
 
 ## Modify the following lines to install dependencies for your project.
 echo "Installing dependencies for non-ROS packages"
-apt update -q
-apt install -y --no-install-recommends git libgoogle-glog-dev \
+sudo apt update -q
+sudo apt install -y --no-install-recommends git libgoogle-glog-dev \
     libgflags-dev \
     libatlas-base-dev \
     libeigen3-dev \
     libsuitesparse-dev
 
 mkdir ./deps
-git clone --depth 1 --branch 2.2.0 https://ceres-solver.googlesource.com/ceres-solver ./deps/ceres-solver
-cd ./deps/ceres-solver
+cd ./deps
+
+git clone --depth 1 --branch 2.2.0 https://ceres-solver.googlesource.com/ceres-solver ./ceres-solver
+cd ./ceres-solver
 mkdir build && cd build
-cmake .. && make -j10 && make install
-cd ../../..
+cmake .. && make -j10 && sudo make install
+cd ../..
+
+git clone --depth 1 --branch v1.x https://github.com/gabime/spdlog.git ./spdlog
+cd ./spdlog && mkdir build && cd build
+cmake .. && make -j10 && sudo make install
+cd ../..
+
+cd ../
 rm -rf ./deps
